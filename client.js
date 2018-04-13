774dbd3719a2767ebd5cb8be7f436d7edfec5703
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-04-13 16:33:06 -0500

    totalMonthlySalary updates to the DOM

--- client.js
+++ client.js
@@ -38,10 +38,7 @@ function updateTotalMonthlySalary(newEmployeeAnnualSalary) {
     totalMonthlySalary += newEmployeeAnnualSalary/12;
     console.log(totalMonthlySalary);
     console.log('function updateMonthlySalary called');
-        $('#totalMonthlySalaryOutput').text('');
-
-    // $('#totalMonthlySalaryOutput').val('Total Monthly: $' + totalMonthlySalary);
-
+    $('#totalMonthlySalaryOutput').text('Total Monthly: $' + totalMonthlySalary);
 }
 
 function clearInputs() {
