0ccbb90565c81b7e58164757ecc54e30522b7505
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-04-13 16:06:43 -0500

    submit button appends input values to employee table

--- client.js
+++ client.js
@@ -2,6 +2,8 @@ console.log('in js');
 
 $(document).ready(onReady);
 
+let totalMonthlySalary = 0;
+
 function onReady() {
     console.log('in jquery');
     $('#submitButton').on('click', clickHandler);
@@ -15,6 +17,19 @@ function clickHandler() {
 function addNewEmployee() {
     console.log('function addNewEmployee called');
     // updateTotalMonthlySalary();
+    let newEmployeeFirstName = $('#firstNameInput').val();
+    let newEmployeeLastName = $('#lastNameInput').val();
+    let newEmployeeID = $('#idInput').val();
+    let newEmployeeTitle = $('#titleInput').val();
+    let newEmployeeAnnualSalary = $('#annualSalaryInput').val();
+    $('#employeeInfoTable').append(                
+    '<tr>' + 
+        '<td>' + newEmployeeFirstName + '</td>' +
+        '<td>' + newEmployeeLastName + '</td>' +
+        '<td>' + newEmployeeID + '</td>' +
+        '<td>' + newEmployeeTitle + '</td>' +
+        '<td>' + newEmployeeAnnualSalary + '</td>' +
+    '</tr>');
     updateTotalMonthlySalary();
 }
 
