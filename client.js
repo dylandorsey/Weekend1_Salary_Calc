44d4118c5adf92add9545dc50c861249c2c4a321
Dylan Dorsey <dylan.dorsey@gmail.com>
2018-04-13 15:58:43 -0500

    clicking calls function train: (clickHandler, addNewEmployee, updateTotalMonthlySalary

--- client.js
+++ client.js
@@ -4,4 +4,21 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('in jquery');
+    $('#submitButton').on('click', clickHandler);
+}
+
+function clickHandler() {
+    console.log('addemployee clicked');
+    addNewEmployee();
+}
+
+function addNewEmployee() {
+    console.log('function addNewEmployee called');
+    // updateTotalMonthlySalary();
+    updateTotalMonthlySalary();
+}
+
+function updateTotalMonthlySalary() {
+    console.log('function updateMonthlySalary called');
+
 }
